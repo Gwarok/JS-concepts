bc12d862f5267c1e64909e3ea88c6a409d1b35ca
Gwarok <gwarokebiro@gmail.com>
2024-01-22 13:14:20 +0300

    created day1 concepts

---
+++ Week1/Day1.js
@@ -0,0 +1,53 @@
+/** @var
+ * you can redeclare
+ * var name = 'jane'
+ * var name = 'john'
+ * you can reassign a variable
+ * var name = 'jane'
+ * name= 'john'
+ *
+ */
+
+/** @let
+ * you can't redeclare
+ * you can reassign
+ * let name = 'jane'
+ * name = 'john'
+ * 
+ */
+
+/** @const
+ * you can't redeclare
+ * you can't reassign
+ * 
+ */
+
+// DATA TYPES
+/**PRIMATIVE DATA TYPES
+ * @STRINGS - characters/numbers wrapped in quotes -> 'dan', "dam", '123'
+ * @NUMBERS - decimal, whole numbers, negtive numbers -> 1, 1.5, -1
+ * @BOOLEANS - true or false
+ * @UNDEFINED - no value assigned
+ * @NULL - nothing
+ */
+
+/**NON-PRIMATIVE DATA TYPES
+ * @LISTS - arrays [1,2,3,4,5], ['dan','james','john']
+ * @OBJECTS - dictionaries (key/value pairs)
+ */
+
+//OPERATORS
+/**
+ * @ADDITION -> +
+ * @SUBTRACTION -> -
+ * @DIVISION -> /
+ * @MULTIPILICATION -> *
+ * @MODULUS -> %
+ */
+
+//SALES CALCULATIONS
+
+//parseInt
+//parseFloat
+
+let length = prompt('length of rectangle?')
