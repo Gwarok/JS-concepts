a22e0b9a69f4cdde59dcc047fedc8d1a060eddaf
Gwarok <gwarokebiro@gmail.com>
2024-01-23 13:49:13 +0300

    day 2 concepts loops and logical operators

---
+++ Week1/Day2.js
@@ -0,0 +1,58 @@
+//let school = 'Zindua'
+
+//if (school === 'Zindua') {
+//    console.log('Zindua School')
+//} else{
+//    console.log('Not Zindua School')
+//}
+
+/**
+ * LOGICAL OPERATORS
+ * @strictlyequal -> ===
+ * @and -> &&
+ * @or -> ||
+ * @greaterthan -> >
+ * @lessthan -> <
+ * @greaterthanorequalto -> >=
+ * @lessthanorequalto -> <=
+ * @not -> !
+ * @notequal -> !==
+ */
+
+// SWITCH STATEMENT
+// let name = 'DOG'
+// switch (name){
+//     case 'isaac':
+//         console.log('i am isaac')
+//         break;
+//     case 'gwaro':
+//         console.log('i am gwaro')
+//         break;
+//     default:
+//         console.log('i am not isaac or gwaro')
+//         break;        
+// }
+
+// FUNCTIONS
+// function consoleHello() { //function declaration
+ 
+//     console.log('hello') //function definition
+// }
+
+// consoleHello() //function call/invocation
+
+// FOR LOOPS
+// for (let i=0; i<10; i++) {
+//     console.log(i)
+// }
+let n= prompt('enter number')
+let sum= 0
+for (m=1; m<n;m++){
+    let p= m%3
+    let q= m%5
+
+    if(p===0 || q===0){
+        sum= sum+m
+    }    
+}
+console.log(sum)
